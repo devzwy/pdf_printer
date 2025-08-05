696d4766af53959205edf509dfb5ad9addee7d31
devzwy <dev_zwy@aliyun.com>
2025-08-05 14:29:23 +0800

    1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,78 +2,71 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.0"
-    `maven-publish`
-    `signing`
+    id("eu.kakde.gradle.sonatype-maven-central-publisher") version "1.0.6"
 }
 
-group = "io.github.devzwy"
-version = "0.0.1"
-
-val sourceJar by tasks.registering(Jar::class) {
-    from(sourceSets["main"].allSource)
-    archiveClassifier.set("sources")
+// ------------------------------------
+// PUBLISHING TO SONATYPE CONFIGURATION
+// ------------------------------------
+object Meta {
+    val COMPONENT_TYPE = "java" // "java" or "versionCatalog"
+    val GROUP = "io.github.devzwy"
+    val ARTIFACT_ID = "pdf_printer"
+    val VERSION = "1.0.1"
+    val PUBLISHING_TYPE = "AUTOMATIC" // USER_MANAGED or AUTOMATIC
+    val SHA_ALGORITHMS = listOf("SHA-256", "SHA-512") // sha256 and sha512 are supported but not mandatory. Only sha1 is mandatory but it is supported by default.
+    val DESC = "打印pdf文件"
+    val LICENSE = "Apache-2.0"
+    val LICENSE_URL = "https://opensource.org/licenses/Apache-2.0"
+    val GITHUB_REPO = "devzwy/pdf_printer.git"
+    val DEVELOPER_ID = "devzwy"
+    val DEVELOPER_NAME = "devzwy"
+    val DEVELOPER_ORGANIZATION = "devzwy"
+    val DEVELOPER_ORGANIZATION_URL = "https://www.y60.net"
 }
 
-tasks.withType<Javadoc> {
-    options {
-        encoding = "UTF-8"
-    }
-}
+val sonatypeUsername: String? by project // this is defined in ~/.gradle/gradle.properties
+val sonatypePassword: String? by project // this is defined in ~/.gradle/gradle.properties
 
-val javadocJar by tasks.creating(Jar::class) {
-    classifier = "javadoc"
-    from(tasks.getByName<Javadoc>("javadoc"))
-}
+sonatypeCentralPublishExtension {
+    // Set group ID, artifact ID, version, and other publication details
+    groupId.set(Meta.GROUP)
+    artifactId.set(Meta.ARTIFACT_ID)
+    version.set(Meta.VERSION)
+    componentType.set(Meta.COMPONENT_TYPE) // "java" or "versionCatalog"
+    publishingType.set(Meta.PUBLISHING_TYPE) // USER_MANAGED or AUTOMATIC
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-//            from(components["kotlin"])
+    // Set username and password for Sonatype repository
+    username.set(System.getenv("SONATYPE_USERNAME") ?: sonatypeUsername)
+    password.set(System.getenv("SONATYPE_PASSWORD") ?: sonatypePassword)
 
-            groupId = "io.github.devzwy"
-            artifactId = "pdf_printer"
-            version = project.version as String?
-
-            // 发布 Javadoc JAR
-            artifact(javadocJar)
-            artifact(sourceJar)
-
-            pom {
-                name.set("pdf_printer")
-                description.set("打印pdf文件")
-                url.set("https://github.com/devzwy/pdf_printer")
-
-                scm {
-                    url.set("https://github.com/devzwy/pdf_printer.git")  // 添加 SCM URL
-                    connection.set("scm:git:https://github.com/devzwy/pdf_printer.git")
-                    developerConnection.set("scm:git:https://github.com/devzwy/pdf_printer.git")
-                }
-
-                licenses {
-                    license {
-                        name.set("The Apache Software License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("dev_zwy")
-                        name.set("devzwy")
-                        email.set("dev_zwy@aliyun.com")
-                    }
-                }
+    // Configure POM metadata
+    pom {
+        name.set(Meta.ARTIFACT_ID)
+        description.set(Meta.DESC)
+        url.set("https://github.com/${Meta.GITHUB_REPO}")
+        licenses {
+            license {
+                name.set(Meta.LICENSE)
+                url.set(Meta.LICENSE_URL)
             }
         }
-    }
-    repositories {
-        maven {
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = project.findProperty("mavenUser") as String? ?: System.getenv("MAVEN_USERNAME")
-                password = project.findProperty("mavenPassword") as String? ?: System.getenv("MAVEN_PASSWORD")
+        developers {
+            developer {
+                id.set(Meta.DEVELOPER_ID)
+                name.set(Meta.DEVELOPER_NAME)
+                organization.set(Meta.DEVELOPER_ORGANIZATION)
+                organizationUrl.set(Meta.DEVELOPER_ORGANIZATION_URL)
             }
+        }
+        scm {
+            url.set("https://github.com/${Meta.GITHUB_REPO}")
+            connection.set("scm:git:https://github.com/${Meta.GITHUB_REPO}")
+            developerConnection.set("scm:git:https://github.com/${Meta.GITHUB_REPO}")
+        }
+        issueManagement {
+            system.set("GitHub")
+            url.set("https://github.com/${Meta.GITHUB_REPO}/issues")
         }
     }
 }
@@ -94,10 +87,6 @@ repositories {
     maven {
         url = uri("https://repo.maven.apache.org/maven2/")
     }
-}
-
-configure<SigningExtension> {
-    sign(publishing.publications["mavenJava"])
 }
 
 dependencies {
--- src/main/kotlin/io/github/devzwy/pdf_printer/PdfPriter.kt
+++ src/main/kotlin/io/github/devzwy/pdf_printer/PdfPriter.kt
@@ -1,9 +1,34 @@
 package io.github.devzwy.pdf_printer
 
+import org.apache.pdfbox.Loader
+import org.apache.pdfbox.printing.PDFPrintable
+import org.apache.pdfbox.printing.Scaling
+import java.awt.print.PrinterJob
+import java.io.File
+import javax.print.PrintServiceLookup
+
 object PdfPriter {
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        println("PDF Printer is running...")
+    fun doPrintByDefault(pdfFilePath: String, showPageBorder: Boolean = false, dpi: Float = 0F, showPrintDialog: Boolean = false) {
+        val file = File(pdfFilePath)
+        Loader.loadPDF(file).use { document ->
+            PrinterJob.getPrinterJob().apply {
+                jobName = "print pdf file ${file.name}"
+                PrintServiceLookup.lookupDefaultPrintService()?.let {
+                    printService = it
+                }
+                java.awt.print.Book().let { book ->
+                    val pageable = PDFPrintable(document, Scaling.SHRINK_TO_FIT, showPageBorder, dpi)
+                    book.append(pageable, defaultPage())
+                    setPageable(book)
+
+                    if (showPrintDialog) {
+                        printDialog()
+                    } else {
+                        print()
+                    }
+                }
+            }
+        }
     }
 }
